256bc28828cbdc075d29e2bda5a76284d3acd26e
tospens <tsmogensen@gmail.com>
2022-02-04 08:03:17 +0100

    [Keymap] Small change to ristretto keymap

--- keyboards/ristretto/keymaps/tospens/keymap.c
+++ keyboards/ristretto/keymaps/tospens/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T              , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , TD(TD_AA) ,
 		KC_LSFT , KC_A    , KC_S    , KC_D    , KC_F    , KC_G              , KC_H    , KC_J    , KC_K    , KC_L    , TD(TD_AE) , TD(TD_OE) ,
 		KC_TAB  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B              , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT  ,
-		KC_CCCV  , KC_LGUI , KC_LALT , KC_LCTL ,MO(LOWER),KC_SPC  , KC_MUTE , KC_ENT  , MO(RAISE) ,KC_BSPC_DEL, KC_RALT , KC_RGUI , KC_RCTRL
+		KC_CCCV  , KC_LALT , KC_LGUI , KC_LCTL ,MO(LOWER),KC_SPC  , KC_MUTE , KC_ENT  , MO(RAISE) ,KC_BSPC_DEL, KC_RGUI , KC_RALT , KC_RCTRL
 	),
 	[GAME] = LAYOUT(
 		KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T              , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , TD(TD_AA) ,
