51f955575e0688c2f8be45c72664cf57a5075fe0
tospens <tsmogensen@gmail.com>
2022-06-12 22:52:33 +0200

    Add tospens_custom keymap to Charybdis 3x5

---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/tospens_custom/rules.mk
@@ -0,0 +1,17 @@
+SRC += features/achordion.c
+SRC += features/caps_word.c
+SRC += features/select_word.c
+SRC += features/layer_lock.c
+
+# Space saving settings.
+TAP_DANCE_ENABLE = no
+QMK_SETTINGS = no
+SPACE_CADET_ENABLE = no
+RGB_ENABLE = no
+RGB_MATRIX_ENABLE = no
+
+# Used features.
+COMBO_ENABLE = yes
+
+# Via settings.
+VIA_ENABLE = yes
